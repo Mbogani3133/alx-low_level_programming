9de3965e4b3d8684617f1cec5fa95a1cea7e3b48
Mbogani3133 <brianmachele@gmail.com>
2022-03-27 22:59:04 +0300

    My commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,7 @@ printf("last digit of %i is %i and is greater then 5\n", n, n % 10);
 }
 
 else if (n % 10 == 0)
+}
 printf("last digit of %i is %i and is 0\n", n, n % 10);
 }
 
@@ -29,5 +30,4 @@ printf("last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
 }
 
 return(0);
-
 }
