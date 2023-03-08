d7c411ef375abf4accdfc8897692e95482845790
Mbogani3133 <brianmachele@gmail.com>
2023-03-09 02:05:56 +0300

    Returning the fuctorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include  "main.h"
+
+/**
+* fuctorial - Returning the factorial of a given number.
+* @n: number input
+*Return: Factorial of the number.
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return(1);
+	else
+		return (n * factorial(n - 1));
+}
