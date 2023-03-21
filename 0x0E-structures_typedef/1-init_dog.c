f16a46dae39ad0cb883550e38317832fb3587a3e
Mbogani3133 <brianmachele@gmail.com>
2023-03-22 02:13:41 +0300

    update on 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,19 +4,19 @@
  * init_dog - initialize dog struct
  *
  * @d: input pointer to struct dog
- * @name: input for dog's name
- * @age: input for dog's age
- * @owner: input for the owner 0f the dog.
+ * @name: input for dog name
+ * @age: input for dog age
+ * @owner: input for dog owner
  *
- * Return: zero (0)
+ * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-	  (*d).name = name;
-	        d->name = name;
+		/*(*d).name = name;*/
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
